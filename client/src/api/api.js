663d4f79cb3dc491480df8d612259410b1aab204
Bhuvenesh-Sheth <shethbhuvenesh@gmail.com>
2024-06-23 17:12:36 +0530

    backedn uri

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const uri = "https://server2-coq34vu10-bhuvenesh-sheths-projects.vercel.app";
+export const uri = "https://server-roxiler.vercel.app/";
 
 export const endpoint =  {
   get_transactions : "/get_transactions",
